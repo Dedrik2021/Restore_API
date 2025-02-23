65416313bf8bc35aa2477a6705ba7e3a00145b20
Oleg Grunski <oleggr578@gmail.com>
2025-02-24 00:03:51 +0100

    Add ExceptionMiddleware to handle exceptions globally

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,14 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddCors();
+builder.Services.AddTransient<ExceptionMiddleware>();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000", "https://localhost:3001"));
 
 app.MapControllers();
